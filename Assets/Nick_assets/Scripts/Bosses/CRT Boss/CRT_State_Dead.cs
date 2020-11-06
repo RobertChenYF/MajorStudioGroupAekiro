e7698d54ba6ae20f409ccfaec2ab0e2972224040
Nick <nickychirico@mac.com>
2020-11-06 18:07:39 -0500

    boss1 - state machine mk4 - stunned / dead / combo States & tweaks

---
+++ Assets/Nick_assets/Scripts/Bosses/CRT Boss/CRT_State_Dead.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRT_State_Dead : CRT_State
+{
+    public override void DoState()
+    {
+    }
+
+    public override void Enter()
+    {
+        Boss.sp.color = Color.gray;
+    }
+
+    public override void Leave()
+    {
+    }
+
+    public CRT_State_Dead(CRT_StateManager myStateManager, CRT_Boss myBoss, string myName, float myDur) : base(myStateManager, myBoss, myName, myDur)
+    {
+    }
+}
